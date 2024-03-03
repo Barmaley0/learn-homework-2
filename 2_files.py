c166e12546d9fa181258ff089ecce272a37aaadc
Den <skornss@mail.ru>
2024-03-03 18:44:24 +0300

    Решено date and files

--- 2_files.py
+++ 2_files.py
@@ -16,7 +16,13 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open(file="referat.txt", mode="r", encoding="utf-8") as text:
+        my_text = text.read()
+        print(f"Количество слов в тексте: {len(my_text.split())}.")
+        print(f"Длинна строки: {len(my_text)} символа.")
+    with open(file="referat2.txt", mode="w", encoding="utf-8") as text_rec:
+        text_rep = my_text.replace(".", "!")
+        text_rec.write(text_rep)
 
 if __name__ == "__main__":
     main()
