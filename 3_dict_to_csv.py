9d11b58c4404ee42daf2f917fa9667a7a9785df8
Den <skornss@mail.ru>
2024-03-04 11:05:43 +0300

    Решено

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -9,13 +9,26 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+person  = [
+    {"name": "Bob", "age": 28, "job": "Baker"},
+    {"name": "Tom", "age": 30, "job": "Driver"},
+    {"name": "John", "age": 20, "job": "Bartender"},
+    {"name": "Mark", "age": 40, "job": "Teacher"},
+]
+
+with open(file="users.csv", mode='w', encoding="utf-8", newline="") as table:
+    fields = ["name", "age", "job"]
+    writer = csv.DictWriter(table, fields, delimiter=",")
+    writer.writeheader()
+    for user in person:
+        writer.writerow(user)
 
 if __name__ == "__main__":
     main()
